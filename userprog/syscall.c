c8c612556b7b3a7f3715d608435bac19b5db1843
Kasun Prabashwara <kasunprabashwara24@gmail.com>
2023-01-02 17:59:57 +0530

    added dummy system calls

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -13,8 +13,65 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
-{
-  printf ("system call!\n");
-  thread_exit ();
+syscall_handler(struct intr_frame* f){
+  switch(*(int*)f->esp){
+    case SYS_HALT:{
+      printf("halt");
+      break;
+    }
+    case SYS_EXIT:{
+      printf("exit");
+      break;
+    }
+    case SYS_EXEC:{
+      printf("exec");
+      break;
+    }
+    case SYS_WAIT:{
+      printf("wait");
+      break;
+    }
+    case SYS_CREATE:{
+      printf("create");
+      break;
+    }
+    case SYS_REMOVE:{
+      printf("remove");
+      break;
+    }
+    case SYS_OPEN:{
+      printf("open");
+      break;
+    }
+    case SYS_FILESIZE:{
+      printf("filesize");
+      break;
+    }
+    case SYS_READ:{
+      printf("read");
+      break;
+    }
+    case SYS_WRITE:{
+      int fd = *((int*)f->esp + 1);
+      void* buffer = (void*)(*((int*)f->esp + 2));
+      unsigned size = *((unsigned*)f->esp + 3);
+      //run the syscall, a function of your own making
+      //since this syscall returns a value, the return value should be stored in f->eax
+      // f->eax = write(fd, buffer, size);
+      printf("write");
+      break;
+    }
+    case SYS_SEEK:{
+      printf("seek");
+      break;
+    }
+    case SYS_TELL:{
+      printf("tell");
+      break;
+    }
+    case SYS_CLOSE:{
+      printf("close");
+      break;
+    }
+  }
 }
